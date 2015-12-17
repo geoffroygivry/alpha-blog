9308e032507e4cbb9208f3e287c340ba31c48753
The Alpha-blog project <geoff.givry@gmail.com>
2015-12-17 08:33:43 -0500

    creation of edit and show views and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,35 @@
 class ArticlesController < ApplicationController
+  
   def new
     @article = Article.new
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated!"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end     
+  end
+  
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(:article)
+    if @article.save
+      flash[:notice] = "Article was successfully created!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end    
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
