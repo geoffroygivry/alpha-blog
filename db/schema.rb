1e77d929014ec0d7252e6fba276fd6b781450df9
The Alpha-blog project <geoff.givry@gmail.com>
2015-12-22 08:22:27 -0500

    add relation between article and user

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222005823) do
+ActiveRecord::Schema.define(version: 20151222032756) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
