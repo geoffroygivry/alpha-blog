a26c8fa7f0a8c3a1acde539563e53cc2d19e500b
The Alpha-blog project <geoff.givry@gmail.com>
2015-12-22 09:40:43 -0500

    added password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }, 
   length: {maximum: 105}, format: { with: VALID_EMAIL_REGEX }
   
+  has_secure_password
+  
 end
