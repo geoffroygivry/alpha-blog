f849b501ba37658a52b06b5ff3ea8df6497ac1d6
The Alpha-blog project <geoff.givry@gmail.com>
2015-12-23 21:24:14 -0500

    added user's show page with their related articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,31 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Alpha Blog! #{@user.username}"
+      flash[:success] = "Welcome to the Alpha Blog #{@user.username}!"
       redirect_to articles_path
     else
       render 'new'
     end
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
   
   private
   def user_params
