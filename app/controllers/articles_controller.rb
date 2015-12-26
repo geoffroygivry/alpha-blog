ff3751019632b31d0b8f856547ef4fada8a97c1a
The Alpha-blog project <geoff.givry@gmail.com>
2015-12-26 08:23:36 -0500

    added admin user table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,7 +56,7 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :description)
     end
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "you can't edit or delete this article"
         redirect_to root_path
       end
