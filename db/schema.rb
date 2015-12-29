9b07f9d65d1776ea3e77d07ce039c5e8cfc5a23e
The Alpha-blog project <geoff.givry@gmail.com>
2015-12-28 21:36:32 -0500

    added test simulation and categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151226131046) do
+ActiveRecord::Schema.define(version: 20151228021748) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20151226131046) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
