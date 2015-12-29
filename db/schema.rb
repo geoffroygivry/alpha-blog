a0aeb67163d28c7085dbd469116f633509d1d6b1
The Alpha-blog project <geoff.givry@gmail.com>
2015-12-29 03:55:48 -0500

    added categoryArticle model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228021748) do
+ActiveRecord::Schema.define(version: 20151229083931) do
+
+  create_table "article_categories", force: true do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: true do |t|
     t.string   "title"
